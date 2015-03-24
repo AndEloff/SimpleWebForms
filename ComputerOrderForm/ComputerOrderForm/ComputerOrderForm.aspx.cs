a7b98c91bd1650704fa4997ad0e7da7f98356436
Andrew Eloff <andeloff@gmail.com>
2015-03-23 23:36:16 -0500

    Created Computer Order Form ASP.NET Web Form
    TODO: Create a button to return the user from the review page back to
    the order page to place another order. Also set up database to connect
    too for storing data of computer orders.

---
+++ ComputerOrderForm/ComputerOrderForm/ComputerOrderForm.aspx.cs
@@ -0,0 +1,36 @@
+﻿using ComputerOrderForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ComputerOrderForm
+{
+    public partial class ComputerOrderForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            //created an order variable to represent the ComputerOrder class to access its properites 
+            var order = new ComputerOrder();
+            //Usually for the ID this would be connected up to a database with a table ID. I am not doing that here but should in real practice
+            order.OrderId = 101;
+            order.OrderName = OrderName.Text;
+            order.Customer = Customer.Text;
+            order.CustomerEmail = CustomerEmail.Text;
+            order.DeliveryDate = DeliveryDate.SelectedDate;
+            order.PartNumber = int.Parse(PartNumber.Text); //parsing the text into an int for the order property 
+            order.Rush = RushYes.Checked;
+            //Stashing in Session State - preserve the data during the session 
+            Session["CurrentOrder"] = order;
+            //after submitting the computer order, the user will be redirected to the read only review page
+            Response.Redirect("ReviewComputerOrder.aspx");
+        }
+    }
+}
---
+++ ComputerOrderForm/ComputerOrderForm/ReviewComputerOrder.aspx.cs
@@ -0,0 +1,31 @@
+﻿using ComputerOrderForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ComputerOrderForm
+{
+    public partial class ReviewComputerOrder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // created order varaible to retreive the Session state CurrentOrder and setting it as type Computer Order
+            //General Overview:
+            //Created order to represent the Session of Current Order
+            //to allow the data in the current session to be binded to the read only text fields on the review page
+            ComputerOrder order = Session["CurrentOrder"] as ComputerOrder;
+            OrderNumber.Text = order.OrderId.ToString();
+            OrderName.Text = order.OrderName;
+            Customer.Text = order.Customer;
+            CustomerEmail.Text = order.CustomerEmail;
+            DeliveryDate.Text = order.DeliveryDate.ToShortDateString();
+            PartNumber.Text = order.PartNumber.ToString();
+            RushOrder.Text = order.Rush == true ? "Yes" : "No";
+
+            //TODO: Create a button to process a new order
+        }
+    }
+}
