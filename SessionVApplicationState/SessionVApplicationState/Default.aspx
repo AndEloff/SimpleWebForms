67ca85d2ddd7073988b2490551569638c1c8f4a6
Andrew Eloff <andeloff@gmail.com>
2015-03-24 20:10:51 -0500

    More Simple Projects
    Added Session vs Application project and a simple project about Cookies

---
+++ SessionVApplicationState/SessionVApplicationState/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SessionVApplicationState.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="NameLabel" runat="server" Text="Enter Your Name: "></asp:Label>
+        <asp:TextBox ID="UserName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="EmailLabel" runat="server" Text="Enter Your Email: "></asp:Label>
+        <asp:TextBox ID="UserEmail" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="SaveBtn" runat="server" Text="Save" OnClick="SaveBtn_Click" />
+        <asp:Button ID="RetrieveBtn" runat="server" Text="Retrieve" OnClick="RetrieveBtn_Click" />
+    
+        <br />
+        <asp:Label ID="NameAndEmail" runat="server" Text=""></asp:Label>
+    
+        <br />
+        <asp:Label ID="RetrieveNameAndEmail" runat="server" Text=""></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ SessionVApplicationState/SessionVApplicationState/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionVApplicationState
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SaveBtn_Click(object sender, EventArgs e)
+        {
+            Session["Name"] = UserName.Text; //Session is tied to a particular browser
+            Application["Email"] = UserEmail.Text; //Application is tied to the entire application KEY DIFFERENCE between Session
+            NameAndEmail.Text = UserName.Text + " " + UserEmail.Text;
+        }
+
+        protected void RetrieveBtn_Click(object sender, EventArgs e)
+        {
+            string name = "";
+            string email = "";
+
+            if (Session["Name"] != null)
+            {
+                name = Session["Name"].ToString();
+            }
+
+            if (Application["Email"] != null)
+            {
+                email = Application["Email"].ToString();
+            }
+
+            RetrieveNameAndEmail.Text = "Retrieved: " + name + " " + email; 
+        }
+    }
+}
