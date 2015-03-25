dad19695858475f16ca2037693bff6fb9d5ec9d9
Andrew Eloff <andeloff@gmail.com>
2015-03-24 23:10:16 -0500

    WebControls Web Form
    Web form that shows off use of different web controls. Needs CSS love

---
+++ WebControls/WebControls/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebControls.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:CheckBox ID="SSD" Text ="Soild State Drive?" runat="server"/>
+        <asp:CheckBox ID="SixteenGB" Text="16GB?" runat="server" />
+        <asp:CheckBox ID="DualMonitor" Text="Dual Monitor?" runat="server" />
+        <hr />
+        <asp:RadioButton ID="Male" Text="Male" GroupName ="Sex" runat="server" Checked ="true" />
+        <asp:RadioButton ID="Female" Text="Female" GroupName="Sex" runat="server" />
+        <hr />
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+            <asp:ListItem Text ="Power Windows" Value ="Feature1" />
+            <asp:ListItem Text ="Power Seats" Value ="Feature2" />
+            <asp:ListItem Text ="Fog Lights" Value ="Feature3" />
+            <asp:ListItem Text ="Winter Package" Value ="Feature4" />
+        </asp:CheckBoxList>
+        <hr />
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+            <asp:ListItem Text ="Under 30" Value ="Young" Selected ="True" />
+            <asp:ListItem Text ="30 - 60" Value ="Young"/>
+            <asp:ListItem Text ="Over 60" Value ="Young"/>
+        </asp:RadioButtonList>
+        <hr />
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem Text="Batman" Value ="Batman" />
+            <asp:ListItem Text="Superman" Value ="Superman" />
+            <asp:ListItem Text="Spiderman" Value ="Spiderman" />
+            <asp:ListItem Text="Static Shock" Value ="Static Shock" />
+            <asp:ListItem Text="Dare Devil" Value ="Dare Devil" />
+        </asp:DropDownList>
+        <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        <asp:Label ID="Msg2" runat="server" Text=""></asp:Label>
+        <asp:Label ID="Message" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebControls/WebControls/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebControls
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //assigned local bool var for check box var
+            bool ssd = SSD.Checked;
+            bool ram = SixteenGB.Checked;
+            bool dual = DualMonitor.Checked;
+            //Set the text using the tunary operator and setting the text to blank if the bool is false
+            Msg2.Text = "<br /> You Selected: ";
+            Msg2.Text += ssd ? "SSD Drive " : "";
+            Msg2.Text += ram ? " 16GB RAM " : "";
+            Msg2.Text += dual ? " Duel Monitors" : "";
+
+            //check box list
+            //Iterate through all the check box list items and if its checked we add it to the message text
+            foreach (ListItem li in CheckBoxList1.Items)
+            {
+                if (li.Selected)
+                {
+                    //taking hte list item and taking its value and coverting it to a string
+                    Message.Text += li.Value.ToString();
+                }
+            }
+
+            //radio buttons
+            Message.Text = "<br /> The customer is: ";
+            //Since there is only 2 values I was able to say its going to be Male OR Female
+            Message.Text += Male.Checked ? "Male was selected" : "Female";
+            Message.Text += "<br /> Your age group is: ";
+
+            //radio list items
+            foreach (ListItem li in RadioButtonList1.Items)
+            {
+                if (li.Selected)
+                {
+                    //if the radio button is selected, append the value we get from that list item
+                    Message.Text += li.Value.ToString() + ". ";
+                }
+            }
+
+            //dropdown list
+            Message.Text += "<br /> You Selected: ";
+            Message.Text += DropDownList1.SelectedValue.ToString();
+        }
+    }
+}
